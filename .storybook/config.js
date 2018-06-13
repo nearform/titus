44489219454c950d7c6e93e348dd7eff8e7933cd
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2018-06-13 15:01:40 +0100

    lint .storybook files

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
 
-const req = require.context('../src/', true, /.story.js$/);
+const req = require.context('../src/', true, /.story.js$/)
 
 function loadStories() {
-  req.keys().forEach(req);
+  req.keys().forEach(req)
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
