1635290ef0c68118bd5d055679fe7ae1b5a3f587
Chris Draycott-Wheatley <chrisdwheatley@users.noreply.github.com>
2018-07-02 15:31:53 +0100

    Add initial starter cli setup (#24)
    * add initial starter cli setup
    
    * fix argument helper
    
    * switch from multiple console.log's to single multiline template string
    
    * tweak colors & weight

--- packages/titus-cli/titus-cli.js
+++ packages/titus-cli/titus-cli.js
@@ -2,8 +2,15 @@
 'use strict'
 
 const program = require('commander')
+const dedent = require('dedent')
+const fs = require('fs-extra')
+const chalk = require('chalk')
+const ora = require('ora')
+const git = require('simple-git/promise')()
 
 const { version } = require('./package.json')
+
+const REPO_URL = 'git@github.com:nearform/titus.git'
 
 program
   .version(version, '-v, --version')
@@ -11,8 +18,40 @@ program
 program
   .command('starter')
   .description('Clone the starter shell')
-  .action(() => {
-    console.log('This is where the magic happens.')
+  .arguments('<project-directory>')
+  .action(async (projectDir) => {
+    let spinner
+
+    try {
+      spinner = ora(
+        `Setting up the Titus starter application in ${chalk.cyan.bold(projectDir)}`
+      ).start()
+
+      await git.clone(REPO_URL, `${projectDir}/.tmp`)
+      await fs.copy(`${projectDir}/.tmp/packages/titus-starter`, `${projectDir}`)
+      await fs.remove(`${projectDir}/.tmp`)
+
+      spinner.succeed(chalk.green.bold(`Setup complete! \n`))
+
+      console.log(dedent`
+        Move to your newly created project by running:
+
+          ${chalk.cyan.bold(`cd ${projectDir}`)}
+
+        Install the project dependencies:
+
+          ${chalk.cyan.bold('npm install')}
+
+        Start the development server by running:
+
+          ${chalk.cyan.bold('npm start')}
+
+        Feedback is welcome at https://github.com/nearform/titus/issues
+      `)
+    } catch (error) {
+      spinner.fail(error)
+      console.log('\n If this issue persists please raise an issue at https://github.com/nearform/titus/issues')
+    }
   })
 
 program.parse(process.argv)
