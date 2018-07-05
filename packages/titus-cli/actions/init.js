fb93ad9a151752f9701af96dc57329e9d96ef70e
Dan Cork <dancork@gmail.com>
2018-07-05 15:29:04 +0100

    add option to clone backend

---
+++ packages/titus-cli/actions/init.js
@@ -0,0 +1,58 @@
+const chalk = require('chalk')
+const dedent = require('dedent')
+const fs = require('fs-extra')
+const kebabCase = require('lodash.kebabcase')
+const ora = require('ora')
+const git = require('simple-git/promise')()
+
+const REPO_URL = 'git@github.com:nearform/titus.git'
+
+module.exports = async (input, { hapi, react }) => {
+  const projectDir = kebabCase(input)
+
+  const spinner = ora()
+
+  const tmpDir = `.${projectDir}-tmp`
+
+  try {
+    spinner.start('Pulling files from GitHub')
+    await git.clone(REPO_URL, tmpDir)
+    spinner.succeed('Pulled files from GitHub')
+
+    if (react) {
+      spinner.render().start(`Setting up app in ${chalk.cyan.bold(`${projectDir}-app`)}`)
+      await fs.copy(`${tmpDir}/packages/titus-starter`, `${projectDir}-app`)
+      spinner.succeed(`App setup in ${chalk.cyan.bold(`${projectDir}-app`)}`)
+    }
+
+    if (hapi) {
+      spinner.render().start(`Setting up api in ${chalk.cyan.bold(`${projectDir}-api`)}`)
+      await fs.copy(`${tmpDir}/packages/titus-backend`, `${projectDir}-api`)
+      spinner.succeed(`Api setup in ${chalk.cyan.bold(`${projectDir}-api`)}`)
+    }
+
+    spinner.render().start('Clearing temporary cache')
+    await fs.remove(`${tmpDir}`)
+    spinner.succeed('Cache cleared')
+
+    console.log(dedent`
+
+      Move to your newly created project by running:
+
+        ${chalk.cyan.bold(`cd ${projectDir}`)}
+
+      Install the project dependencies:
+
+        ${chalk.cyan.bold('npm install')}
+
+      Start the development server by running:
+
+        ${chalk.cyan.bold('npm start')}
+
+      Feedback is welcome at https://github.com/nearform/titus/issues
+    `)
+  } catch (error) {
+    spinner.fail(error)
+    console.log('\n If this issue persists please raise an issue at https://github.com/nearform/titus/issues')
+  }
+}
