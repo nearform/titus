4c6342d8114172c83b9f8897ea82b57ea7df8399
Matt Robinson <matt.robinson@nearform.com>
2018-07-11 15:18:21 +0100

    Extracted table row into a component

--- packages/titus-kitchen-sink/src/containers/api/table/material/material-ui-table.js
+++ packages/titus-kitchen-sink/src/containers/api/table/material/material-ui-table.js
@@ -1,26 +1,13 @@
-import React, { Fragment, Component } from 'react'
-import {
-  TableHeaderRow as NfTableHeaderRow,
-  TableHeader as NfTableHeader
-} from '@nearform/react-table'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import TableRow from '@material-ui/core/TableRow'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TextField from '@material-ui/core/TextField'
 import TablePagination from '@material-ui/core/TablePagination'
 import Paper from '@material-ui/core/Paper'
-import Select from '@material-ui/core/Select'
-import Button from '@material-ui/core/Button'
-import MenuItem from '@material-ui/core/MenuItem'
-import Checkbox from '@material-ui/core/Checkbox'
-import EditIcon from '@material-ui/icons/Edit'
-import ResetIcon from '@material-ui/icons/Clear'
-import SaveIcon from '@material-ui/icons/Check'
 import TableToolbar from './table-toolbar'
-import HeaderRow from './header-row'
-import SortingHeaderCell from './sorting-header-cell'
+import Header from './header'
+import Row from './table-row'
 
 class MaterialUiTable extends Component {
   static propTypes = {
@@ -39,33 +26,10 @@ class MaterialUiTable extends Component {
     handlePageSizeChange: PropTypes.func
   }
 
-  state = {
-    rowChanges: {}
-  }
-
   handleDelete = () => {
     const { onDelete, rows } = this.props
     const toDelete = rows.filter(({ selected }) => selected)
     onDelete(toDelete)
-  }
-
-  handleUpdate = data => () => {
-    this.props.onUpdate(data)
-  }
-
-  handleEdit = (id, accessor) => e => {
-    // Keep all changes in component state until saved or reverted
-    this.setState({
-      rowChanges: {
-        ...this.state.rowChanges,
-        [id]: { ...this.state.rowChanges[id], [accessor]: e.target.value }
-      }
-    })
-  }
-
-  cancelEdit = id => e => {
-    const { [id]: value, ...rowChanges } = this.state.rowChanges
-    this.setState({ rowChanges })
   }
 
   handleRowSelect = event => {
@@ -77,17 +41,6 @@ class MaterialUiTable extends Component {
       event.target.value = page + 1 // material ui is 0 offset so adjust for nf-table
       this.props.handlePageChangeBlur(event)
     }
-  }
-
-  onEditClick = rowData => () => {
-    // Copy normalised data to component state, where it will be edited from
-    const row = rowData.reduce((acc, curr) => {
-      if (curr.accessor) {
-        acc[curr.accessor] = curr.data
-      }
-      return acc
-    }, {})
-    this.setState({ rowChanges: { ...this.state.rowChanges, [row.id]: row } })
   }
 
   render () {
@@ -115,114 +68,29 @@ class MaterialUiTable extends Component {
           numSelected={selecting[0] === 'all' ? total : selecting.length}
         />
         <Table>
-          <NfTableHeaderRow component={HeaderRow}>
-            {[
-              <NfTableHeader key='select-all'>
-                <TableCell padding='checkbox'>
-                  <Checkbox
-                    color='primary'
-                    value='all'
-                    onClick={handleRowSelect}
-                    checked={selecting[0] === 'all'}
-                  />
-                </TableCell>
-              </NfTableHeader>,
-              ...columns.reduce((acc, curr, index) => {
-                if (curr.accessor) {
-                  acc.push(
-                    <NfTableHeader
-                      {...curr}
-                      key={index}
-                      component={SortingHeaderCell}
-                    >
-                      {curr.label}
-                    </NfTableHeader>
-
-                  )
-                }
-                return acc
-              }, [])
-            ]}
-          </NfTableHeaderRow>
-
+          <Header
+            columns={columns}
+            handleRowSelect={handleRowSelect}
+            selecting={selecting}
+          />
           <TableBody>
-            {rows.map(({ rowKey, rowData, selected }, index) => {
+            {rows.map(({ rowKey, rowData, selected }) => {
               const row = rowData.reduce((acc, curr) => {
                 if (curr.accessor) {
                   acc[curr.accessor] = curr.data
                 }
                 return acc
               }, {})
-              const isRowEditing = !!this.state.rowChanges[row.id]
-
               return (
-                <TableRow
-                  hover
-                  role='checkbox'
-                  aria-checked={selected}
-                  tabIndex={-1}
+                <Row
                   key={rowKey}
                   selected={selected}
-                >
-                  <TableCell padding='checkbox' key={`${rowKey}-checkbox`}>
-                    <Checkbox
-                      color='primary'
-                      value={rowKey}
-                      checked={selected}
-                      onClick={handleRowSelect}
-                    />
-                  </TableCell>
-                  <TableCell padding='checkbox' key={`${rowKey}-name`}>
-                    {!isRowEditing && row.name}
-                    {isRowEditing && (
-                      <TextField
-                        fullWidth
-                        defaultValue={row.name}
-                        onChange={this.handleEdit(row.id, 'name')}
-                      />
-                    )}
-                  </TableCell>
-                  <TableCell padding='checkbox' key={`${rowKey}-foodGroup`}>
-                    {!isRowEditing && row.foodGroup}
-                    {isRowEditing && (
-                      <Select
-                        autoWidth
-                        value={this.state.rowChanges[row.id].foodGroupId}
-                        onChange={this.handleEdit(row.id, 'foodGroupId')}
-                      >
-                        {foodGroups.map(o => (
-                          <MenuItem key={o.id} value={o.id}>
-                            {o.name}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    )}
-                  </TableCell>
-                  {!isRowEditing && (
-                    <Fragment>
-                      <TableCell padding='checkbox'>
-                        <Button onClick={this.onEditClick(rowData)}>
-                          <EditIcon />
-                        </Button>
-                      </TableCell>
-                      <TableCell />
-                    </Fragment>
-                  )}
-                  {isRowEditing && (
-                    <Fragment>
-                      <TableCell padding='checkbox'>
-                        <Button onClick={this.cancelEdit(row.id)}>
-                          <ResetIcon />
-                        </Button>
-                      </TableCell>
-                      <TableCell padding='checkbox'>
-                        <Button onClick={this.handleUpdate(this.state.rowChanges[row.id])}>
-                          <SaveIcon />
-                        </Button>
-                      </TableCell>
-                    </Fragment>
-                  )}
-                </TableRow>
+                  rowKey={rowKey}
+                  row={row}
+                  foodGroups={foodGroups}
+                  handleRowSelect={handleRowSelect}
+                  handleUpdate={this.props.onUpdate}
+                />
               )
             })}
             <TableRow>
