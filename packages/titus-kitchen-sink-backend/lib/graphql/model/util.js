4c48051e1872b26160751b76e8d666bd8322b35e
Cian Foley <cian.foley@nearform.com>
2018-08-03 10:48:43 +0100

    comment in util for tsquery

--- packages/titus-kitchen-sink-backend/lib/graphql/model/util.js
+++ packages/titus-kitchen-sink-backend/lib/graphql/model/util.js
@@ -24,11 +24,16 @@ const arrayToDict = (arr = []) => {
 }
 
 const toTsQuery = query => {
+  // here we're essentially breaking all the terms down in the search making each
+  // word a prefix match using :* and tying them together using & operator
+  // the regular expressions ensure single spacing before split, and that
+  // embedded quotes and backslashes must be doubled within the search term
+  // e.g. brian o'brien => 'brian':* & 'o''brien':*
+  // see https://www.postgresql.org/docs/9.1/static/datatype-textsearch.html
   let tsQuery = query.trim()
-
   if (tsQuery !== '') {
     tsQuery =
-      "'" +
+      "'" + //
       tsQuery
         .replace(/ +/g, ' ')
         .replace(/\\/g, '\\\\')
