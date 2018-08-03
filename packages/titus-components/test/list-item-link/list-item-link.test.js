4bb3059925b21f273c9bad72186bb4f5be978eef
Cian Foley <cian.foley@nearform.com>
2018-08-03 12:12:07 +0100

    tests were fixed in another pr, removing dev dep

--- packages/titus-components/test/list-item-link/list-item-link.test.js
+++ packages/titus-components/test/list-item-link/list-item-link.test.js
@@ -4,9 +4,13 @@ import { render } from 'react-testing-library'
 describe('ListItemLink', () => {
   describe('rendering', () => {
     test('Default rendering', () => {
-      jest.mock('@reach/router', () => ({
-        Link: () => <div data-testid='custom-link' />
-      }))
+      jest.mock(
+        '@reach/router',
+        () => ({
+          Link: () => <div data-testid='custom-link' />
+        }),
+        { virtual: true }
+      )
       const ListItemLink = require('../../src/list-item-link/list-item-link')
         .default
       const { getByTestId } = render(<ListItemLink />)
