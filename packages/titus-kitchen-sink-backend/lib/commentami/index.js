a44fe1ac14be75b65478f2fc0030966fddfab983
Jimmy Mintzer <jimmymintzer@users.noreply.github.com>
2018-09-11 17:20:49 -0400

    cleanup

--- packages/titus-kitchen-sink-backend/lib/commentami/index.js
+++ packages/titus-kitchen-sink-backend/lib/commentami/index.js
@@ -27,11 +27,8 @@ const passwords = {
 }
 
 const validate = async (request, username, password, h) => {
-  if (username === 'help') {
-    return { response: h.redirect('https://hapijs.com/help') } // custom response
-  }
-
   const user = users[username]
+
   if (!user) {
     return { credentials: null, isValid: false }
   }
