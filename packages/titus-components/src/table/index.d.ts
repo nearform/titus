2efcb4c561a488dfbf7f43ac2084c3040128936f
Peter Hiža <peter.hiza@gmail.com>
2018-09-24 12:37:24 +0200

    [Issue-107]: Add typings to titus-components (#152)

---
+++ packages/titus-components/src/table/index.d.ts
@@ -0,0 +1,2 @@
+export { default, default as Table } from './table'
+export * from './table'
---
+++ packages/titus-components/src/uploader/index.d.ts
@@ -0,0 +1,4 @@
+export { default, default as Uploader } from './uploader'
+export { default as UploaderService } from './services'
+export * from './uploader'
+export * from './services'
