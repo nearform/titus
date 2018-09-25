4b442de38f3747f88ca20af57a22d6b8252110e9
Peter Hiža <peter.hiza@gmail.com>
2018-09-25 11:03:05 +0200

    Documentation ported to use Docz

--- docs/kitchen-sink.md
+++
@@ -1,5 +0,0 @@
-Titus includes a sample application called kitchen-sink. It is composed of a frontend and backend package. 
-
-The [frontend](kitchen-sink-frontend.md) is built on [create-react-app](), with the addition of [Storybook](), [Material-UI](), [Reach Router](), [Apollo]() and more. It includes demos of open source client libraries we use, NearForm's own open source projects and design system. It also shows how to use the React components contained in the [titus-components](titus-components.md) package.
-
-The [backend](kitchen-sink-backend.md) is built on [Hapi](https://hapijs.com/), [PostgreSQL](https://www.postgresql.org/), [Docker](https://www.docker.com/), [GraphQL](https://graphql.org/) and more. It also includes demos of open source backend libraries and NearForm's open source packages.
---
+++ packages/titus-documentation/doczrc.js
@@ -0,0 +1,44 @@
+import * as path from 'path'
+import externalLinks from 'remark-external-links'
+import remarkBookmarks from 'remark-bookmarks'
+
+const bookmarks = {
+  '@nearform/react-table': 'https://github.com/nearform/react-table',
+  'create-react-app': 'https://github.com/facebook/create-react-app',
+  'Material UI': 'https://material-ui.com/',
+  'Reach Router': 'https://reach.tech/router',
+  'react-apollo': 'https://github.com/apollographql/react-apollo',
+  'react-dnd': 'https://github.com/react-dnd/react-dnd',
+  'titus-components': '/titus-components',
+  Apollo: 'https://www.apollographql.com/',
+  Docker: 'https://www.docker.com/',
+  downshift: 'https://github.com/paypal/downshift',
+  GraphQL: 'https://graphql.org/',
+  Hapi: 'https://hapijs.com/',
+  PostgreSQL: 'https://www.postgresql.org/',
+  Stepper: 'https://material-ui.com/demos/steppers/',
+  Storybook: 'https://storybook.js.org/'
+}
+
+const SRC = path.resolve(__dirname, 'src')
+const PACKAGES = path.resolve(__dirname, '../')
+const ROOT = path.resolve(__dirname, '../../')
+
+export default {
+  description: 'This is the official documentation of @nearform/titus monorepo',
+  mdPlugins: [externalLinks.default, [remarkBookmarks, { bookmarks }]],
+  src: PACKAGES,
+  files: `{*/src/*.mdx,*/src/**/*.mdx}`,
+  dest: `../../docs`,
+  hashRouter: true,
+  modifyBundlerConfig: config => {
+    config.resolve.alias = Object.assign({}, config.resolve.alias, {
+      // '@fonts': `${PUBLIC}/fonts`,
+      // '@images': `${PUBLIC}/images`,
+      // '@components': `${ROOT}/packages/titus-components/es`,
+      '@styles': `${SRC}/theme/styles`
+    })
+
+    return config
+  }
+}
