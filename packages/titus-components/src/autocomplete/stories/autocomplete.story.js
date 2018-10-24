651798e18d3535c2518aadb4a6fb601c610fea4f
Stuart Wilson <stuart.wilson@nearform.com>
2018-10-24 15:50:27 +0100

    [112] - Add storybook example of autocomplete component with child

--- packages/titus-components/src/autocomplete/stories/autocomplete.story.js
+++ packages/titus-components/src/autocomplete/stories/autocomplete.story.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import Typography from '@material-ui/core/Typography';
 
 import { countries } from './countries'
 import Autocomplete from '../autocomplete'
@@ -12,4 +13,13 @@ storiesOf('Autocomplete', module)
       onChange={action('onChange')}
       data={countries}
     />
+  ))
+  .add('with helper text', () => (
+    <Autocomplete
+      placeholder="I am a placeholder"
+      onChange={action('onChange')}
+      data={countries}
+    >
+      <Typography variant="caption">Start typing to see all options</Typography>
+    </Autocomplete>
   ))
