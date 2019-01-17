1d69d139b6c34fff5889d17d22c5887262064497
David Gonzalez <dagonza1983@gmail.com>
2019-01-17 17:14:26 +0000

    Titus Fastify now lives on its own repository

--- packages/titus-fastify-backend/lib/db-client/diet-type/index.js
+++
@@ -1,9 +0,0 @@
-const getAll = require('./get-all')
-const deleteDietType = require('./delete')
-const update = require('./update')
-
-module.exports = {
-  getAll,
-  delete: deleteDietType,
-  update
-}
