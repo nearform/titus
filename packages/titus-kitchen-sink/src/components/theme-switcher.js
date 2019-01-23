da7cc3e07f249bb33586dc2f568a132b6abac9ce
Cristian Kiss <cristian.kiss@nearform.com>
2019-01-23 15:18:18 +0200

    revert theme-switcher to old code

--- packages/titus-kitchen-sink/src/components/theme-switcher.js
+++ packages/titus-kitchen-sink/src/components/theme-switcher.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo'
 import IconButton from '@material-ui/core/IconButton'
 import InvertColorsIcon from '@material-ui/icons/InvertColors'
 import { loader } from 'graphql.macro'
@@ -7,20 +7,24 @@ import { THEME } from '../theme'
 
 const query = loader('../theme/query.graphql')
 
-const ThemeSwitcher = ({ data: { themeName }, client }) => (
-  <IconButton
-    title={`Switch to ${themeName === THEME.default ? THEME.nearform : THEME.default} theme`}
-    color="inherit"
-    onClick={() =>
-      client.writeData({
-        data: {
-          themeName: themeName === THEME.default ? THEME.nearform : THEME.default
+const ThemeSwitcher = () => (
+  <Query query={query}>
+    {({ data: { themeName }, client }) => (
+      <IconButton
+        title={`Switch to ${themeName === THEME.default ? THEME.nearform : THEME.default} theme`}
+        color="inherit"
+        onClick={() =>
+          client.writeData({
+            data: {
+              themeName: themeName === THEME.default ? THEME.nearform : THEME.default
+            }
+          })
         }
-      })
-    }
-  >
-    <InvertColorsIcon />
-  </IconButton>
+      >
+        <InvertColorsIcon />
+      </IconButton>
+    )}
+  </Query>
 )
 
-export default graphql(query)(ThemeSwitcher)
+export default ThemeSwitcher
