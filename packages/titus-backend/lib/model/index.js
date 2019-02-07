10a8ac62cd76d2501895c327a236aabd63683226
Cristian Kiss <cristian.kiss@nearform.com>
2019-02-07 16:36:09 +0200

    some code tidy up

--- packages/titus-backend/lib/model/index.js
+++ packages/titus-backend/lib/model/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
 module.exports = {
-  loaders: pg => {
-    return {}
-  }
+  loaders: () => ({})
 }
--- packages/titus-backend/lib/model/util.js
+++ packages/titus-backend/lib/model/util.js
@@ -2,34 +2,33 @@
 
 const camelcase = require('camelcase')
 
-const formatRows = rows => {
-  return rows.map(row => {
+const formatRows = rows =>
+  rows.map(row => {
     return Object.keys(row).reduce((acc, fieldName) => {
       acc[camelcase(fieldName)] = row[fieldName]
       return acc
     }, {})
   })
-}
 
 const sortByIdArray = (rows, ids) => {
   const dict = arrayToDict(rows)
   return ids.map(id => dict[id])
 }
 
-const arrayToDict = (arr = []) => {
-  return arr.reduce((dict, obj = {}) => {
+const arrayToDict = (arr = []) =>
+  arr.reduce((dict, obj = {}) => {
     dict[obj.id] = obj
     return dict
   }, {})
-}
 
+/** here we're essentially breaking all the terms down in the search making each
+ * word a prefix match using :* and tying them together using & operator
+ * the regular expressions ensure single spacing before split, and that
+ * embedded quotes and backslashes must be doubled within the search term
+ * e.g. brian o'brien => 'brian':* & 'o''brien':*
+ * see https://www.postgresql.org/docs/9.1/static/datatype-textsearch.html
+ */
 const toTsQuery = query => {
-  // here we're essentially breaking all the terms down in the search making each
-  // word a prefix match using :* and tying them together using & operator
-  // the regular expressions ensure single spacing before split, and that
-  // embedded quotes and backslashes must be doubled within the search term
-  // e.g. brian o'brien => 'brian':* & 'o''brien':*
-  // see https://www.postgresql.org/docs/9.1/static/datatype-textsearch.html
   let tsQuery = query.trim()
   if (tsQuery !== '') {
     tsQuery =
