e5d26f16b3b972820214a051bc054edc095ddaa2
Cristian Kiss <cristian.kiss@nearform.com>
2019-02-12 17:49:17 +0200

    added initial version of health check endpoint

--- packages/titus-backend/lib/routes/index.js
+++ packages/titus-backend/lib/routes/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
 const error = require('./error')
+const health = require('./rest/health')
 
-module.exports = (server, config) => [].concat(error(server, config))
+module.exports = (server, config) =>
+  [].concat(error(server, config), health(server, config))
