619ed7a155136ecac87f4b2fea90b2c4cc09cdcb
Joe Warren <joe.warren@nearform.com>
2019-02-12 09:46:58 +0000

    Update storybook with theme and addons.

---
+++ packages/titus-frontend/src/components/login/components/login-form.story.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import LoginForm from './login-form'
+
+storiesOf('Login form', module).add(
+  'default',
+  () => <LoginForm header="Titus login form" />,
+  {
+    info: {
+      text: `
+      ## Login Form
+      
+      This is the form used on the login page. 
+      It uses \`formik\` to manage the form which means you can pass it a validation schema and it will do the work for you.
+      `
+    }
+  }
+)
