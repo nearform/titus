3a1b277edfa56fc59f29c5a4df7eb69a4443016d
ron litzenberger <ron.litzenberger@nearform>
2019-02-15 11:17:33 -0600

    make lint happy

--- packages/titus-backend/tools/migrate.js
+++ packages/titus-backend/tools/migrate.js
@@ -2,22 +2,21 @@
 require('dotenv').config()
 const Postgrator = require('postgrator')
 const migrate = async () => {
-try{
-  const action = process.argv[2] || 'build' 
-const postgress = await new Postgrator({
-  migrationDirectory: `${__dirname}/migrations/${action}`,
-  driver: 'pg',
-  host: process.env.PGHOST || 'postgres',
-  port: process.env.PGPORT || '5432',
-  database: process.env.POSTGRES_DB || 'postgres',
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  schemaTable: `schemaversion_${action}`
-})
-
- const appliedMigrations = await postgress.migrate()
- console.log(appliedMigrations)
-} catch(error){
+  try {
+    const action = process.argv[2] || 'build'
+    const postgress = await new Postgrator({
+      migrationDirectory: `${__dirname}/migrations/${action}`,
+      driver: 'pg',
+      host: process.env.PGHOST || 'postgres',
+      port: process.env.PGPORT || '5432',
+      database: process.env.POSTGRES_DB || 'postgres',
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      schemaTable: `schemaversion_${action}`
+    })
+    const appliedMigrations = await postgress.migrate()
+    console.log(appliedMigrations)
+  } catch (error) {
     console.error(error)
     process.exit()
   }
