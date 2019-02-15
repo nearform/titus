a70873077ea552dec674135b71424e4d030ffcfd
ron litzenberger <ron.litzenberger@nearform>
2019-02-15 03:06:35 -0600

    fix: migration script for local dev

--- packages/titus-backend/tools/migrate.js
+++ packages/titus-backend/tools/migrate.js
@@ -1,22 +1,26 @@
 'use strict'
-
+require('dotenv').config()
 const Postgrator = require('postgrator')
-
-new Postgrator({
-  migrationDirectory: `${__dirname}/migrations/${process.argv[2]}`,
+const migrate = async () => {
+try{
+  const action = process.argv[2] || 'build' 
+const postgress = await new Postgrator({
+  migrationDirectory: `${__dirname}/migrations/${action}`,
   driver: 'pg',
   host: process.env.PGHOST || 'postgres',
   port: process.env.PGPORT || '5432',
   database: process.env.POSTGRES_DB || 'postgres',
   username: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
-  schemaTable: `schemaversion_${process.argv[2]}`
+  schemaTable: `schemaversion_${action}`
 })
-  .migrate()
-  .then(appliedMigrations => {
-    console.log(appliedMigrations)
-  })
-  .catch(error => {
+
+ const appliedMigrations = await postgress.migrate()
+ console.log(appliedMigrations)
+} catch(error){
     console.error(error)
     process.exit()
-  })
+  }
+}
+
+module.exports = migrate()
