94117e5d3f3083f656cf9862bdfebb1bafc145cb
ron litzenberger <ron.litzenberger@gmail.com>
2019-02-17 11:01:32 -0600

    Updates for sample app (#347)
    * fix: migration script for local dev
    
    * added empty migration scripts for starter
    
    * added starter migration file for seed data
    
    * added starter migration file for seed dev
    
    * removed seed_data directory.  Use seed_dev
    
    * make lint happy

--- packages/titus-backend/tools/migrate.js
+++ packages/titus-backend/tools/migrate.js
@@ -1,22 +1,25 @@
 'use strict'
-
+require('dotenv').config()
 const Postgrator = require('postgrator')
-
-new Postgrator({
-  migrationDirectory: `${__dirname}/migrations/${process.argv[2]}`,
-  driver: 'pg',
-  host: process.env.PGHOST || 'postgres',
-  port: process.env.PGPORT || '5432',
-  database: process.env.POSTGRES_DB || 'postgres',
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  schemaTable: `schemaversion_${process.argv[2]}`
-})
-  .migrate()
-  .then(appliedMigrations => {
+const migrate = async () => {
+  try {
+    const action = process.argv[2] || 'build'
+    const postgress = await new Postgrator({
+      migrationDirectory: `${__dirname}/migrations/${action}`,
+      driver: 'pg',
+      host: process.env.PGHOST || 'postgres',
+      port: process.env.PGPORT || '5432',
+      database: process.env.POSTGRES_DB || 'postgres',
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      schemaTable: `schemaversion_${action}`
+    })
+    const appliedMigrations = await postgress.migrate()
     console.log(appliedMigrations)
-  })
-  .catch(error => {
+  } catch (error) {
     console.error(error)
     process.exit()
-  })
+  }
+}
+
+module.exports = migrate()
