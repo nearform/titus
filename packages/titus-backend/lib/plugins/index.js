22385f76968c7c3c9002e73a655a6eef91edd605
feugy <damien.feugas@gmail.com>
2019-02-18 17:48:23 +0100

    remove unused depenedencies, fix npm warnings

--- packages/titus-backend/lib/plugins/index.js
+++ packages/titus-backend/lib/plugins/index.js
@@ -2,10 +2,7 @@ const config = require('../../config/default')
 
 module.exports = [
   // logging first because other plugins may use it
-  {
-    plugin: require('./logger'),
-    options: config
-  },
+  { plugin: require('./logger'), options: config },
   // then authentication because it declares default strategy for incoming routes
   {
     plugin: require('./auth0'),
