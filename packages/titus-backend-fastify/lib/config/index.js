bdf28fbbd224ebb8faa3d72444f1b0c013174660
Thiago Kroger <thiagokroger@gmail.com>
2019-08-06 17:43:45 -0300

    Add env-schema and fluent-schema to config

--- packages/titus-backend-fastify/lib/config/index.js
+++ packages/titus-backend-fastify/lib/config/index.js
@@ -1,26 +1,27 @@
 'use strict'
+const envSchema = require('env-schema')
+const S = require('fluent-schema')
 
-require('dotenv-expand')(require('dotenv').config())
+const config = envSchema({
+  dotenv: true,
+  schema: S.object()
+    .prop('NODE_ENV', S.string().required())
+    .prop('API_HOST', S.string().required())
+    .prop('API_PORT', S.string().required())
+    .prop('PGHOST', S.string().required())
+    .prop('PGPORT', S.string().required())
+    .prop('PGUSER', S.string().required())
+    .prop('PGPASSWORD', S.string().required())
+    .prop('PGDATABASE', S.string().required())
+    .prop('AUTH0_DOMAIN', S.string())
+    .prop('AUTH0_CLIENT_ID', S.string())
+    .prop('AUTH0_CLIENT_SECRET', S.string())
+    .prop('AUTH0_AUDIENCE', S.string())
+    .prop('AUTH0_GRANT_TYPE', S.string())
+    .prop('JTW_SECRET', S.string().default('3000'))
+})
 
-const {
-  API_HOST,
-  API_PORT,
-  PGHOST,
-  PGPORT,
-  PGUSER,
-  PGPASSWORD,
-  PGDATABASE,
-  NODE_ENV,
-  CORS_ORIGIN,
-  AUTH0_DOMAIN,
-  AUTH0_CLIENT_ID,
-  AUTH0_CLIENT_SECRET,
-  AUTH0_AUDIENCE,
-  AUTH0_GRANT_TYPE,
-  JTW_SECRET
-} = process.env
-
-const isProduction = /^\s$production\s*$/i.test(NODE_ENV)
+const isProduction = /^\s$production\s*$/i.test(config.NODE_ENV)
 
 /**
  * Global configuration, from env variables
@@ -29,35 +30,35 @@ module.exports = {
   isProduction,
   // Fastify options: https://www.fastify.io/docs/latest/Server/:
   fastify: {
-    host: API_HOST,
-    port: +API_PORT
+    host: config.API_HOST,
+    port: +config.API_PORT
   },
   fastifyInit: {
     logger: true
   },
   // pg-pool options: https://github.com/brianc/node-pg-pool#create
   pgPlugin: {
-    host: PGHOST,
-    port: +PGPORT,
-    database: PGDATABASE,
-    user: PGUSER,
-    password: PGPASSWORD,
+    host: config.PGHOST,
+    port: +config.PGPORT,
+    database: config.PGDATABASE,
+    user: config.PGUSER,
+    password: config.PGPASSWORD,
     poolSize: 10,
     idleTimeoutMillis: 30000
   },
   // under pressure options: https://github.com/fastify/under-pressure
   underPressure: {},
   // cors options: https://github.com/fastify/fastify-cors
-  cors: { origin: !!CORS_ORIGIN },
+  cors: { origin: !!config.CORS_ORIGIN },
   // auth0 plugin options: see plugins/auth0/index.js
   auth0: {
-    domain: `https://${AUTH0_DOMAIN}`,
-    clientId: AUTH0_CLIENT_ID,
-    clientSecret: AUTH0_CLIENT_SECRET,
-    audience: AUTH0_AUDIENCE,
-    grantType: AUTH0_GRANT_TYPE
+    domain: `https://${config.AUTH0_DOMAIN}`,
+    clientId: config.AUTH0_CLIENT_ID,
+    clientSecret: config.AUTH0_CLIENT_SECRET,
+    audience: config.AUTH0_AUDIENCE,
+    grantType: config.AUTH0_GRANT_TYPE
   },
   jwt: {
-    secret: JTW_SECRET
+    secret: config.JTW_SECRET
   }
 }
