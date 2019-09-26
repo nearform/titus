8a523581a6880767dbfedb52da45a1deb61f9429
RudyRed <will.gross@nearform.com>
2019-09-26 10:43:59 +0100

    Make fastify primary backend

--- packages/titus-backend/lib/server.test.js
+++ packages/titus-backend/lib/server.test.js
@@ -1,27 +1,10 @@
-const axios = require('axios')
-const startServer = require('./server')
-const config = require('./config')
+'use strict'
 
 describe('server', () => {
-  let server
-
-  afterEach(async () => {
-    if (server) {
-      await server.stop()
-    }
-  })
-
-  it('starts a server handling 404', async () => {
-    server = await startServer({
-      ...config,
-      'hapi-pino': {
-        ...config['hapi-pino'],
-        level: 'silent'
-      }
-    })
-    const response = await axios.get(`${server.info.uri}/unknown`, {
-      validateStatus: () => true
-    })
-    expect(response.status).toBe(404)
+  it('starts a server and register plugins', async () => {
+    const server = { register: jest.fn() }
+    server.register.mockReturnValue(server)
+    require('./server')(server, require('./config'))
+    expect(server.register).toHaveBeenCalledTimes(3)
   })
 })
