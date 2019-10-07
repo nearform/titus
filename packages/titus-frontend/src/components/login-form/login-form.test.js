740dff49f2528875b9706bf8804a6ca415866789
Dan Williams <dan@ithium.net>
2019-10-07 10:18:10 +0100

    Feature/361 replace enzyme (#404)
    * Removed enzyme and added react-testing-library packages (#361)
    
      - RTL required updating react-dom to at least 16.9
    
    * Replaced use of enzyme with react-testing-library (#361)

--- packages/titus-frontend/src/components/login-form/login-form.test.js
+++ packages/titus-frontend/src/components/login-form/login-form.test.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { render, fireEvent } from '@testing-library/react'
 import LoginForm from '.'
 
 describe('LoginForm', () => {
-  let container
-
-  afterEach(() => {
-    container.unmount()
-  })
-
-  it('should not submit if username or password are empty', () => {
+  it('should not submit if username or password are empty', async done => {
     const mockFn = jest.fn()
-    container = mount(<LoginForm login={mockFn} />)
-    expect(container.find('button').length).toEqual(1)
-    container.find('button').simulate('click')
-    expect(mockFn).toHaveBeenCalledTimes(0)
+    const { findAllByText, getByText } = render(<LoginForm login={mockFn} />)
+    const loginButtons = await findAllByText('Login')
+    expect(loginButtons.length).toEqual(1)
+    fireEvent.click(getByText('Login'))
+    setTimeout(() => {
+      // give fireEvent time to execute
+      expect(mockFn).toHaveBeenCalledTimes(1)
+      done()
+    }, 1)
   })
 })
