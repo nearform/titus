4eaa36c39cd2ac2c31764023cdd72f8bd31ff014
Thiago <thiagokroger@gmail.com>
2020-01-30 15:16:52 -0300

    Add Frontend to Backend Logging Utility (msg.info) (#414)

--- packages/titus-frontend/src/pino.js
+++ packages/titus-frontend/src/pino.js
@@ -1,23 +1,29 @@
 import pino from 'pino'
 const { REACT_APP_API_PATH } = process.env
 
-const logger = pino({
+const config = {
   browser: {
-    asObject: true,
-    transmit: {
-      level: 'info',
-      send: async (level, logEvent) => {
-        const msg = logEvent.messages[0]
+    asObject: true
+  }
+}
 
-        const headers = {
-          type: 'application/json'
-        }
-        let blob = new Blob([JSON.stringify({ msg })], headers)
+if (REACT_APP_API_PATH) {
+  config.browser.transmit = {
+    level: 'info',
+    send: async (level, logEvent) => {
+      const msg = logEvent.messages[0]
 
-        navigator.sendBeacon(`${REACT_APP_API_PATH}/log`, blob)
+      const headers = {
+        type: 'application/json'
       }
+      let blob = new Blob([JSON.stringify({ msg })], headers)
+
+      navigator.sendBeacon(`${REACT_APP_API_PATH}/log`, blob)
     }
   }
-})
+}
 
-export default logger
+const logger = pino(config)
+const info = msg => logger.info(msg)
+
+export default info
---
+++ packages/titus-frontend/src/pino.test.js
@@ -0,0 +1,19 @@
+import pino from './pino'
+
+jest.mock('pino', () => a => {
+  if (a.browser && a.browser.transmit) {
+    a.browser.transmit.send(0, { messages: ['msg'] })
+  }
+
+  return {
+    info: () => {}
+  }
+})
+
+describe('Common file', () => {
+  test('should trigger pino', () => {
+    pino('test message')
+
+    expect(global.navigator.sendBeacon.mock.calls.length).toBe(1)
+  })
+})
