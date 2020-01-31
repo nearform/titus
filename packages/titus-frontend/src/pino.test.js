fd42d0c0da738b25d3d6975e5069cfdc745d7b76
Thiago <thiagokroger@gmail.com>
2020-01-31 12:03:51 -0300

    Add Frontend to Backend Logging Utility (Pino tests) (#414)

--- packages/titus-frontend/src/pino.test.js
+++ packages/titus-frontend/src/pino.test.js
@@ -1,19 +1,35 @@
-import { log } from './pino'
+import pino, { log } from './pino'
+let warnMessage
+let infoMessage
 
-jest.mock('pino', () => a => {
-  if (a.browser && a.browser.transmit) {
-    a.browser.transmit.send(0, { messages: ['msg'] })
-  }
+jest.mock('pino', params => {
+  return params => {
+    if (params.browser && params.browser.transmit) {
+      params.browser.transmit.send(0, { messages: ['msg'] })
+    }
 
-  return {
-    info: () => {}
+    return {
+      info: msg => {
+        infoMessage = msg
+      },
+      warn: msg => {
+        warnMessage = msg
+      }
+    }
   }
 })
 
-describe('Common file', () => {
-  test('should trigger pino', () => {
-    log('test message')
+describe('Pino file', () => {
+  test('should trigger pino log function', () => {
+    log('log message')
 
     expect(global.navigator.sendBeacon.mock.calls.length).toBe(1)
+    expect(infoMessage).toBe('log message')
+  })
+
+  test('should trigger pino default with warn', () => {
+    pino.warn('warn message')
+
+    expect(warnMessage).toBe('warn message')
   })
 })
