070e0896d184271d07a1b88cd470be8b74388527
Thiago Kroger <thiagokroger@gmail.com>
2020-02-12 06:46:22 -0300

    Auth tests for Azure (#466) (#468)

---
+++ packages/titus-frontend/src/components/auth-providers/azure-ad/adalConfig.test.js
@@ -0,0 +1,40 @@
+describe('Adal config', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('should trigger correctly with process.env', async () => {
+    process.env = {
+      REACT_APP_AD_TENANT: 'tenent',
+      REACT_APP_AD_APP_ID: 'id'
+    }
+    const authContext = require('./adalConfig')
+
+    expect(authContext.adalConfig.tenant).toBe('tenent')
+    expect(authContext.adalConfig.clientId).toBe('id')
+    expect(authContext.adalConfig.endpoints.api).toBe('id')
+
+    try {
+      await authContext.adalApiFetch()
+    } catch (err) {
+      expect(err).toEqual({
+        message: 'User login is required',
+        msg: 'login required'
+      })
+    }
+  })
+
+  it('should trigger correctly without process.env', async () => {
+    process.env = {
+      REACT_APP_AD_TENANT: undefined,
+      REACT_APP_AD_APP_ID: undefined
+    }
+
+    const authContext = require('./adalConfig')
+
+    expect(authContext.adalConfig.clientId).toBe(undefined)
+    expect(authContext.adalConfig.clientId).toBe(undefined)
+    expect(authContext.adalConfig.endpoints.api).toBe(undefined)
+    expect(authContext.authContext).toBe(null)
+  })
+})
