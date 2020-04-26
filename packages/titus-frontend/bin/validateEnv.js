fdcbccd7ff01d66769f1b2f7e5cdedafed87efe8
Cristian Kiss <cristian.kiss@nearform.com>
2020-04-26 19:26:09 +0300

    use fluent-schema and env-schema to validate env variables

--- packages/titus-frontend/bin/validateEnv.js
+++ packages/titus-frontend/bin/validateEnv.js
@@ -1,21 +1,21 @@
-const yup = require('yup')
-
-const schemas = {
-  development: yup.object().shape({
-    REACT_APP_API_PATH: yup.string().required('REACT_APP_API_PATH is required')
-  }),
-
-  production: yup.object().shape({
-    REACT_APP_API_PATH: yup.string().required('REACT_APP_API_PATH is required')
-  })
-}
+const envSchema = require('env-schema')
+const S = require('fluent-schema')
 
 const env = process.env.NODE_ENV || 'development'
 
-schemas[env]
-  .validate(process.env)
-  .then(() => console.info('The env variables are valid'))
-  .catch(err => {
-    console.error(`Environment variables errors: ${err.errors}`)
-    return process.exit(1)
-  })
+const schemas = {
+  development: S.object().prop(
+    'REACT_APP_API_PATH',
+    S.string().minLength(1).required()
+  ),
+
+  production: S.object().prop(
+    'REACT_APP_API_PATH',
+    S.string().minLength(1).required()
+  )
+}
+
+envSchema({
+  schema: schemas[env],
+  dotenv: true
+})
