793e288531549c1988bf6e94349b15dcac9569bb
Cristian Kiss <cristian.kiss@nearform.com>
2020-04-27 13:15:00 +0300

    unit test env validation

---
+++ packages/titus-frontend/src/validateEnv.js
@@ -0,0 +1,26 @@
+const S = require('fluent-schema')
+
+const env = process.env.NODE_ENV || 'development'
+
+const schemas = {
+  development: S.object().prop(
+    'REACT_APP_API_PATH',
+    S.string().minLength(1).required()
+  ),
+
+  production: S.object().prop(
+    'REACT_APP_API_PATH',
+    S.string().minLength(1).required()
+  )
+}
+
+const validate = function () {
+  require('env-schema')({
+    schema: schemas[env],
+    dotenv: true
+  })
+}
+
+module.exports = {
+  validate
+}
---
+++ packages/titus-frontend/src/validateEnv.test.js
@@ -0,0 +1,34 @@
+describe('validateEnv file', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('should succeed if env variables are present', async () => {
+    process.env = {
+      NODE_ENV: 'development',
+      REACT_APP_API_PATH: 'http://localhost'
+    }
+
+    try {
+      const validateResult = require('./validateEnv').validate()
+      expect(validateResult).toEqual(undefined)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+  it('should fail if REACT_APP_API_PATH env variables is missing', async () => {
+    process.env = {
+      NODE_ENV: 'development',
+      REACT_APP_API_PATH: undefined
+    }
+
+    try {
+      require('./validateEnv').validate()
+    } catch (err) {
+      expect(err.message).toEqual(
+        "should have required property 'REACT_APP_API_PATH'"
+      )
+    }
+  })
+})
