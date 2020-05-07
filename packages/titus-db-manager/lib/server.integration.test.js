582b345e1cf246ef873aa6c0ed916455bc21e3ff
ivanjov <ivanjov96@gmail.com>
2020-05-07 21:02:32 +0200

    Change order of tests

--- packages/titus-db-manager/lib/server.integration.test.js
+++ packages/titus-db-manager/lib/server.integration.test.js
@@ -3,28 +3,6 @@ const buildServer = require('./build-server')
 describe('Server Integration', () => {
   const fastify = buildServer()
 
-  describe('/db/truncate', () => {
-    it('runs truncate command in DB', done => {
-      fastify.inject(
-        {
-          method: 'POST',
-          url: '/db/truncate'
-        },
-        (err, response) => {
-          if (err) {
-            return done(err)
-          }
-
-          expect(response.json()).toEqual({
-            success: true
-          })
-
-          done()
-        }
-      )
-    })
-  })
-
   describe('/db/migrate', () => {
     it('runs migrate command in DB', done => {
       fastify.inject(
@@ -53,6 +31,28 @@ describe('Server Integration', () => {
         {
           method: 'POST',
           url: '/db/seed'
+        },
+        (err, response) => {
+          if (err) {
+            return done(err)
+          }
+
+          expect(response.json()).toEqual({
+            success: true
+          })
+
+          done()
+        }
+      )
+    })
+  })
+
+  describe('/db/truncate', () => {
+    it('runs truncate command in DB', done => {
+      fastify.inject(
+        {
+          method: 'POST',
+          url: '/db/truncate'
         },
         (err, response) => {
           if (err) {
