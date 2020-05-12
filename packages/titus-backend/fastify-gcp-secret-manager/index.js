e532f080437e69fed73705cbb5d4a3d86a3ac5f0
Cristian Kiss <cristian.kiss@nearform.com>
2020-05-12 10:12:14 +0300

    fixed package-lock

--- packages/titus-backend/fastify-gcp-secret-manager/index.js
+++ packages/titus-backend/fastify-gcp-secret-manager/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const SecretManagerServiceClient = require('@google-cloud/secret-manager').SecretManagerServiceClient
+const SecretManagerServiceClient = require('@google-cloud/secret-manager')
+  .SecretManagerServiceClient
 const fp = require('fastify-plugin')
 let client
 
@@ -8,13 +9,13 @@ async function accessSecretVersion(name) {
   try {
     const [version] = await client.accessSecretVersion({ name })
     return version.payload.data.toString('utf8')
-  } catch(err) {
-      // the error message from GCP SDK should get propagated to user as it contain path to the secret, project ID, etc..
-      throw new Error('Secret not found')
+  } catch (err) {
+    // the error message from GCP SDK should get propagated to user as it contain path to the secret, project ID, etc..
+    throw new Error('Secret not found')
   }
 }
 
-async function fastifyGCPSecretManager (fastify, options, next) {
+async function fastifyGCPSecretManager(fastify, options, next) {
   const name = options.name
   delete options.name
 
@@ -24,22 +25,28 @@ async function fastifyGCPSecretManager (fastify, options, next) {
 
   const secrets = {}
   for (const key in options) {
-      secrets[key] = await accessSecretVersion(options[key])
-    }
-  
+    secrets[key] = await accessSecretVersion(options[key])
+  }
+
   if (name) {
     if (!fastify.secrets) {
       fastify.decorate('secrets', {})
     }
 
     if (fastify.secrets[name]) {
-      return next(new Error(`fastify-gcp-secret-manager '${name}' instance name has already been registered`))
+      return next(
+        new Error(
+          `fastify-gcp-secret-manager '${name}' instance name has already been registered`
+        )
+      )
     }
 
     fastify.secrets[name] = secrets
   } else {
     if (fastify.secrets) {
-      return next(new Error('fastify-gcp-secret-manager has already been registered'))
+      return next(
+        new Error('fastify-gcp-secret-manager has already been registered')
+      )
     } else {
       fastify.decorate('secrets', secrets)
     }
--- packages/titus-backend/get-secret.js
+++ packages/titus-backend/get-secret.js
@@ -1,17 +1,17 @@
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 
 // Instantiates a client
-const client = new SecretManagerServiceClient();
+const client = new SecretManagerServiceClient()
 
 async function accessSecretVersion() {
   const [version] = await client.accessSecretVersion({
-    name: 'projects/494141678371/secrets/test/versions/latest',
-  });
+    name: 'projects/494141678371/secrets/test/versions/latest'
+  })
 
   // Extract the payload as a string.
-  const payload = version.payload.data.toString('utf8');
+  const payload = version.payload.data.toString('utf8')
 
-  console.info(payload);
+  console.info(payload)
 }
 
-accessSecretVersion();
+accessSecretVersion()
