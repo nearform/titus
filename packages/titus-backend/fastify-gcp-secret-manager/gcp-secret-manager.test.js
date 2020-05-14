800183bf26bcef88575d71f47a2a8d96f7300b87
Cristian Kiss <cristian.kiss@nearform.com>
2020-05-14 11:10:29 +0300

    add some more tests to gcp-secret-manager

--- packages/titus-backend/fastify-gcp-secret-manager/gcp-secret-manager.test.js
+++ packages/titus-backend/fastify-gcp-secret-manager/gcp-secret-manager.test.js
@@ -6,16 +6,21 @@ describe('fastify-gcp-secret-manager', () => {
   beforeAll(async () => {})
 
   beforeEach(async () => {
+    jest.resetModules()
     server = require('fastify')()
+    delete process.env.NODE_ENV
   })
 
   afterEach(async () => server.close())
 
-  afterAll(async () => server.close())
+  afterAll(async () => {
+    server.close()
+    jest.resetModules()
+  })
 
   it('should read secrets from options.developmentSecrets in dev mode', async () => {
     process.env = {
-      NODE_ENV: 'test'
+      NODE_ENV: 'development'
     }
 
     server.register(require('.'), {
@@ -29,6 +34,65 @@ describe('fastify-gcp-secret-manager', () => {
 
     expect(server.secrets).toEqual({
       test: 'I am a test secret for development'
+    })
+  })
+
+  it('should read secrets from options in production mode', async () => {
+    process.env = {
+      NODE_ENV: 'production'
+    }
+
+    jest.mock('@google-cloud/secret-manager', () => ({
+      SecretManagerServiceClient: jest.fn().mockImplementation(() => ({
+        accessSecretVersion: jest
+          .fn()
+          .mockReturnValue([{ payload: { data: 'Hello World!' } }])
+      }))
+    }))
+
+    server.register(require('.'), {
+      test: 'projects/494141678371/secrets/test/versions/latest'
+    })
+
+    await server.listen(5003)
+
+    expect(server.secrets).toEqual({
+      test: 'Hello World!'
+    })
+  })
+
+  it('should throw error when secret is not found in production mode', async () => {
+    process.env = {
+      NODE_ENV: 'production'
+    }
+
+    jest.mock('@google-cloud/secret-manager', () => ({
+      SecretManagerServiceClient: jest.fn().mockImplementation(() => ({
+        accessSecretVersion: jest.fn().mockReturnValue(new Error('not found'))
+      }))
+    }))
+
+    server.register(require('.'), {
+      test: 'secret'
+    })
+
+    await server.listen(5003, err => {
+      expect(err.message).toEqual('Secret not found: secret')
+    })
+  })
+
+  it('should not register gcp-secret-manager plugin twice', async () => {
+    process.env = {
+      NODE_ENV: 'development'
+    }
+
+    server.register(require('.'))
+    server.register(require('.'))
+
+    await server.listen(5003, err => {
+      expect(err.message).toEqual(
+        'fastify-gcp-secret-manager has already been registered'
+      )
     })
   })
 })
