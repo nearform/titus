1794b57ab1aaeb57e968fb4ca67ff04ecc50678e
Chris Draycott-Wheatley <chrisdwheatley@users.noreply.github.com>
2020-06-30 11:28:29 +0100

    fix(storybook): fix storybook warning about cra addon/preset (#557)

--- packages/titus-frontend/.storybook/main.js
+++ packages/titus-frontend/.storybook/main.js
@@ -2,11 +2,17 @@ module.exports = {
   stories: ['../src/**/*.story.(js|mdx)'],
   addons: [
     '@storybook/addon-a11y/register',
-    '@storybook/addon-docs',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    },
     '@storybook/addon-actions/register',
     '@storybook/addon-links/register',
     '@storybook/addon-knobs/register',
     '@storybook/addon-options/register',
+    '@storybook/preset-create-react-app',
     'storybook-readme/register'
   ]
 }
