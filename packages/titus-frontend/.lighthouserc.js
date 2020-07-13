09f9cdea5d575a2b5f34eec339821ba22842f774
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-07-13 17:06:25 +0100

    fix(lighthouse): remove unneccesary rules

--- packages/titus-frontend/.lighthouserc.js
+++ packages/titus-frontend/.lighthouserc.js
@@ -1,14 +1,7 @@
 module.exports = {
   ci: {
     assert: {
-      preset: 'lighthouse:recommended',
-      assertions: {
-        'categories:performance': ['warn', {}],
-        'categories:accessibility': ['warn', {}],
-        'categories:best-practices': ['warn', {}],
-        'categories:seo': ['warn', {}],
-        'categories:pwa': ['warn', {}]
-      }
+      preset: 'lighthouse:recommended'
     }
   }
 }
