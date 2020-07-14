7971cda4ebc57dae706a6880d81e4b2649cf8cc6
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-07-14 09:28:26 +0100

    feat(lighthouse): comment out config

--- packages/titus-frontend/.lighthouserc.js
+++ packages/titus-frontend/.lighthouserc.js
@@ -1,7 +1,13 @@
-module.exports = {
-  ci: {
-    assert: {
-      preset: 'lighthouse:recommended'
-    }
-  }
-}
+/*
+ * uncomment the code below to start running GitHub Status Checks against your pull requests
+ * see https://github.com/GoogleChrome/lighthouse-ci/blob/master/docs/getting-started.md#add-assertions
+ * for details on how to customise rules
+ */
+
+// module.exports = {
+//   ci: {
+//     assert: {
+//       preset: 'lighthouse:recommended'
+//     }
+//   }
+// }
