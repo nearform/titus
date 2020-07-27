4379d6ed02016bdfaca32ab906934dcfd4d0ba95
Adam Davis <adam@admataz.com>
2020-07-27 10:58:23 +0100

    remove check for id_token

--- packages/titus-frontend/src/components/auth-providers/titus-backend/index.js
+++ packages/titus-frontend/src/components/auth-providers/titus-backend/index.js
@@ -15,9 +15,8 @@ export default class Authentication {
       throw Error((await response.json()).message)
     }
     const authResult = await response.json()
-    if (authResult && authResult.access_token && authResult.id_token) {
+    if (authResult && authResult.access_token) {
       localStorage.setItem('access_token', authResult.access_token)
-      localStorage.setItem('id_token', authResult.id_token)
       localStorage.setItem(
         'expires_at',
         authResult.expires_in * 1000 + new Date().getTime()
@@ -28,7 +27,6 @@ export default class Authentication {
 
   async logout() {
     localStorage.removeItem('access_token')
-    localStorage.removeItem('id_token')
     localStorage.removeItem('expires_at')
     return true
   }
