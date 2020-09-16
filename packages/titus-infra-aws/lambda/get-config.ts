0ee37a66ca9156f165ae9a7e9d13aaf9410b0456
Davide Fiorello <davide@codeflyer.com>
2020-09-16 15:21:51 +0200

    feat(deploy): add test to config lambda

--- packages/titus-infra-aws/lambda/get-config.ts
+++ packages/titus-infra-aws/lambda/get-config.ts
@@ -1,24 +1,20 @@
 module.exports = {
   handler: async () => {
     const prefix = 'APP_CONFIG_'
-    try {
-      const returnValues = Object.entries(process.env)
-        .filter(entry => entry[0].startsWith(prefix))
-        .reduce((acc: Record<string, string | undefined>, next) => {
-          acc[next[0].substr(prefix.length)] = next[1]
-          return acc
-        }, {})
-1
-      return {
-        statusCode: 200,
-        body: JSON.stringify(returnValues),
-        headers: {
-          'Access-Control-Allow-Origin': process.env.CORS_ORIGIN,
-          'Access-Control-Allow-Credentials': process.env.CORS_CREDENTIALS === 'true'
-        }
+    const returnValues = Object.entries(process.env)
+      .filter(entry => entry[0].startsWith(prefix))
+      .reduce((acc: Record<string, string | undefined>, next) => {
+        acc[next[0].substr(prefix.length)] = next[1]
+        return acc
+      }, {})
+    1
+    return {
+      statusCode: 200,
+      body: JSON.stringify(returnValues),
+      headers: {
+        'Access-Control-Allow-Origin': process.env.CORS_ORIGIN,
+        'Access-Control-Allow-Credentials': process.env.CORS_CREDENTIALS === 'true'
       }
-    } catch (e) {
-      console.log('Error on lambda call')
     }
   }
 }
