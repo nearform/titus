ff7b0581ebf720372d03ff39c879aeafb1154750
Sergey Kurdin <kurdin@gmail.com>
2020-11-02 17:11:49 -0500

    Eslint fixes and ignoring storybook static folder

--- packages/titus-frontend/config-overrides.js
+++ packages/titus-frontend/config-overrides.js
@@ -1,7 +1,6 @@
 const { override, addBabelPlugins } = require('customize-cra')
 
 const alias = require('./aliases.config')
-const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = override(
   ...addBabelPlugins(
