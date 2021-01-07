f0631542e56bd40eda46238248fdb9972a24f9f1
Adam Davis <adam.davis@nearform.com>
2021-01-07 16:11:15 +0000

    chore: improve AD auth

--- packages/titus-frontend/src/index.js
+++ packages/titus-frontend/src/index.js
@@ -1,9 +1,9 @@
 /*
   Required for `runWithAdal` below:
-
-  import { runWithAdal } from 'react-adal'
-  import { authContext } from 'components/auth-providers/azure-ad/adalConfig'
 */
+// import { runWithAdal } from 'react-adal'
+// import config from 'lib/config'
+// import { getAuthContext } from 'components/auth-providers/azure-ad/adalConfig'
 import React from 'react'
 import { render } from 'react-dom'
 import 'lib/i18n'
@@ -11,17 +11,15 @@ import App from 'app'
 import { register as registerServiceWorker } from 'serviceWorker'
 
 render(<App />, document.getElementById('root'))
-
 /*
   Replace above `render` with the below, to enabled Azure AD authentication
-
-  runWithAdal(
-    authContext,
-    () => {
-      render(<App />, document.getElementById('root'))
-    },
-    false
-  )
 */
+// runWithAdal(
+//   getAuthContext(config),
+//   () => {
+//     render(<App />, document.getElementById('root'))
+//   },
+//   false
+// )
 
 registerServiceWorker()
