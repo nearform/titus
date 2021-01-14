80dd8f9195f034281b234eff9cdc313a10d2fc9f
Adam Davis <adam.davis@nearform.com>
2021-01-14 23:05:36 +0000

    use casbin role based policy - fix tests

--- packages/titus-backend/lib/plugins/auth/authzcheck.js
+++ packages/titus-backend/lib/plugins/auth/authzcheck.js
@@ -5,6 +5,15 @@ const { Forbidden } = require('http-errors')
 const jwt = require('jsonwebtoken')
 
 async function authRoutes(server, options) {
+  // add users to the admin role
+  server.addHook('onReady', async function () {
+    const adminUsers = process.env.CHECK_AUTHZ_ADMIN_USERS || ''
+    const policies = adminUsers
+      .split(',')
+      .map(u => server.casbin.addRoleForUser(u, 'role_admin'))
+    await Promise.all(policies)
+  })
+
   // this is a sample to check the signed-in user's permission to access this resource via casbin policies based on configuration in the OAuth provider
   // a real-world example would have more context around the user account and roles/scopes/permissions and policies
   server.route({
@@ -23,11 +32,11 @@ async function authRoutes(server, options) {
       async (request, res) => {
         const authzId = request.headers['x-authz-id']
         const authzUserData = jwt.decode(authzId)
-        const adminUsers = process.env.CHECK_AUTHZ_ADMIN_USERS || ''
-        const isAdmin = adminUsers.split(',').includes(authzUserData.email)
-
-        // Here's where the actual policy check goes ahead
-        if (!(await server.casbin.enforce({ isAdmin }, 'admin', 'access'))) {
+        // we added the user to role with admin access above in the server.onReady hook
+        // Here's where the actual policy check goes ahead - based on the user's email
+        if (
+          !(await server.casbin.enforce(authzUserData.email, 'admin', 'access'))
+        ) {
           throw new Forbidden('Cannot access admin')
         }
       }
