e54aad7c3de57dc74d3e1f2c1b15879380a23906
Kurt Milam <kurt.milam@xioup.com>
2021-03-08 12:09:22 +0100

    move swagger & helmet registration to titus-backend/lib/server.js

--- packages/titus-backend/index.js
+++ packages/titus-backend/index.js
@@ -3,7 +3,6 @@
 'use strict'
 
 const fastify = require('fastify')
-const helmet = require('fastify-helmet')
 
 const startServer = require('./lib/server')
 const config = require('./lib/config')
@@ -16,18 +15,6 @@ process.on('unhandledRejection', err => {
 
 const main = async () => {
   const server = fastify(config.fastify)
-  server.register(require('fastify-swagger'), require('./lib/config/swagger'))
-  server.register(helmet, instance => ({
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        'form-action': [`'self'`],
-        'img-src': [`'self'`, 'data:', 'validator.swagger.io'],
-        'script-src': [`'self'`].concat(instance.swaggerCSP.script),
-        'style-src': [`'self'`, 'https:'].concat(instance.swaggerCSP.style)
-      }
-    }
-  }))
   server.register(startServer, config)
 
   await server.listen(config.server)
--- packages/titus-backend/lib/server.js
+++ packages/titus-backend/lib/server.js
@@ -2,11 +2,24 @@
 
 const path = require('path')
 
+const helmet = require('fastify-helmet')
 const autoLoad = require('fastify-autoload')
 const fp = require('fastify-plugin')
 
 async function plugin(server, config) {
   server
+    .register(require('fastify-swagger'), require('./config/swagger'))
+    .register(helmet, instance => ({
+      contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          'form-action': [`'self'`],
+          'img-src': [`'self'`, 'data:', 'validator.swagger.io'],
+          'script-src': [`'self'`].concat(instance.swaggerCSP.script),
+          'style-src': [`'self'`, 'https:'].concat(instance.swaggerCSP.style)
+        }
+      }
+    }))
     .register(require('fastify-cors'), config.cors)
     .register(require('fastify-casbin'), config.casbin)
     .register(autoLoad, {
