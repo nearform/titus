a2eb5ba98f2929b89a1df7453161993fab6c4413
Miguel Arias <miguel.arias@nearform.com>
2021-07-09 11:01:44 +0100

    fixing issue after azurerm version upgrade

--- infra/azure/terraform/main.tf
+++ infra/azure/terraform/main.tf
@@ -7,19 +7,20 @@ terraform {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "DevOps_BenchTesting_MA_Jun21"
-    storage_account_name = "titusazure2"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "DevOps_BenchTesting_MA_Jun21"
+#    storage_account_name = "titusazure2"
+#    container_name       = "tfstate"
+#    key                  = "terraform.tfstate"
+#  }
+#}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
   subscription_id = var.resource_group_id
+  skip_provider_registration = true
 }
 
 data "azurerm_client_config" "current" {
