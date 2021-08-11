a1382f3203f10f23ec49a417bfc52c6b2275a487
Leonardo Rossi <leonardo.rossi@gmail.com>
2021-08-11 10:28:34 +0200

    Updates SECRETS_PG_PASS env value (#1020)
    Sets by default `azure` as secrets strategy for containers deployed in Azure
    
    Closes #1004

--- infra/azure/terraform/db-manager.tf
+++ infra/azure/terraform/db-manager.tf
@@ -33,8 +33,8 @@ resource "azurerm_container_group" "titus-db-manager-containergroup" {
       "PG_DATABASE" = azurerm_postgresql_database.titus-db.name
       "PG_USER"     = "${var.db_user}@${azurerm_postgresql_server.titus-db-server.name}"
       "PG_PASSWORD"     = azurerm_key_vault_secret.titus-db-password.value
-      "SECRETS_STRATEGY" = "env"
-      "SECRETS_PG_PASS" = "PG_PASSWORD"
+      "SECRETS_STRATEGY" = "azure"
+      "SECRETS_PG_PASS" = format("%s|%s", azurerm_key_vault.titus-key-vault.id, azurerm_key_vault_secret.titus-db-password.value)
       "JWT_SECRET"  = azurerm_key_vault_secret.titus-jwt-secret.value
       "NODE_ENV"    = "development"
       "AUTH0_DOMAIN" = "dummy"
