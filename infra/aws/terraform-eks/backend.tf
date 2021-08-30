90f4a5935474bc89460e5c92db2c1247abfe47f8
ronan.doolan@williamhill.co.uk <ronan.doolan2@mail.dcu.ie>
2021-08-30 14:31:15 +0100

    remove ecs iam roles

--- infra/aws/terraform-eks/backend.tf
+++ infra/aws/terraform-eks/backend.tf
@@ -56,54 +56,6 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceControlle
 ########################################################################
 # IAM 
 ########################################################################
-data "aws_iam_policy_document" "ecs_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecs_task_execution" {
-  name               = "${var.default_name}-task-exec-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role" "ecs_task_role" {
-  name               = "${var.default_name}-task-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "ecs_task_policy" {
-
-  statement {
-    actions = ["secretsmanager:GetSecretValue"]
-    resources = [
-      aws_secretsmanager_secret.db_password.arn
-    ]
-
-  }
-
-}
-
-resource "aws_iam_policy" "ecs_task_policy" {
-  name   = "${var.default_name}-ecs-task-policy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.ecs_task_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.ecs_task_policy.arn
-}
 #######################################################################
 
 
