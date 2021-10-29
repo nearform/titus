d4018e8d2296cc4b7e6ed4f17946d84e38a2d2f5
Jeremy Sharp <jeremysharp@me.com>
2021-10-29 14:24:28 +0100

    staticCSP for swagger

--- packages/titus-backend-typescript/src/server.ts
+++ packages/titus-backend-typescript/src/server.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 
-import helmet from 'fastify-helmet'
 import swagger from 'fastify-swagger'
 import autoLoad from 'fastify-autoload'
 import fp from 'fastify-plugin'
@@ -15,21 +14,9 @@ const serverPlugin: FastifyPluginAsync<typeof configOptions> = async (
   server,
   config
 ) => {
-  // @ts-expect-error TODO fix
   server
     // swagger must be registered before helmet
     .register(swagger, config.swagger)
-    .register(helmet, ({ swaggerCSP }) => ({
-      contentSecurityPolicy: {
-        directives: {
-          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-          'form-action': [`'self'`],
-          'img-src': [`'self'`, 'data:', 'validator.swagger.io'],
-          'script-src': [`'self'`].concat(swaggerCSP.script),
-          'style-src': [`'self'`, 'https:'].concat(swaggerCSP.style)
-        }
-      }
-    }))
     .register(fastifyCors, config.cors)
     .register(casbin, config.casbin)
     .register(autoLoad, {
