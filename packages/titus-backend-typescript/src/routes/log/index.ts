694b14abc53c700a42545983463a35ecf6beaebd
Jeremy Sharp <jeremysharp@me.com>
2021-11-01 14:49:11 +0000

    typing etc for log router

--- packages/titus-backend-typescript/src/routes/log/index.ts
+++ packages/titus-backend-typescript/src/routes/log/index.ts
@@ -1,33 +1,41 @@
+import { Type, Static } from '@sinclair/typebox'
 import { FastifyPluginAsync } from 'fastify'
 
+const BodyType = Type.Strict(
+  Type.Object({
+    msg: Type.String(),
+    level: Type.KeyOf(
+      Type.Object({
+        trace: Type.String(),
+        debug: Type.String(),
+        info: Type.String(),
+        warn: Type.String(),
+        error: Type.String(),
+        fatal: Type.String()
+      })
+    )
+  })
+)
+
 const log: FastifyPluginAsync = async (server) => {
-  server.route({
+  server.route<{ Body: Static<typeof BodyType> }>({
     method: 'POST',
     url: '/',
     schema: {
-      body: {
-        type: 'object',
-        properties: {
-          msg: { type: 'string' },
-          level: {
-            type: 'string',
-            enum: ['trace', 'debug', 'info', 'warn', 'error', 'fatal']
-          }
-        }
-      },
+      body: BodyType,
       tags: ['log'],
       response: {
-        200: {
-          description: 'Successful response',
-          type: 'object',
-          properties: {
-            message: { type: 'string' }
-          }
-        }
+        200: Type.Strict(
+          Type.Object({
+            message: Type.String({
+              description: 'Successful response'
+            })
+          })
+        )
       }
     },
     handler: async (req) => {
-      const { msg, level = 'info' } = req.body as any
+      const { msg, level = 'info' } = req.body
       req.log[level]({ front: msg })
       return { message: 'logged successfully' }
     }
